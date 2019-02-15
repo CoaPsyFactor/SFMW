26729a686a03cb0933f9f5ed32bbc279e10b8e89
Aleksandar Zivanovic <coapsyfactor@gmail.com>
2019-02-15 22:32:53 +0100

    removed blog example and moved it to another repo

--- blog/src/controllers/post/view.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-return function (int $postId, bool $edit = false): array {
-    $post = \Simple\Database::Fetch('SELECT * FROM `posts` WHERE `id` = :postId;', [':postId' => $postId]);
-
-    return compact('post', 'edit');
-};
